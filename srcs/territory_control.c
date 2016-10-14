b43543b8f29a78a4cd823b5be8d66b723571cd2e
Mickael DOS SANTOS <mdos-san@e2r3p3.42.fr>
2016-10-14 19:49:11 +0200

    getting logs

--- srcs/territory_control.c
+++ srcs/territory_control.c
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/14 17:30:28 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/10/14 18:07:45 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/10/14 19:28:43 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,62 @@ static void	clean(t_filler *filler)
 	}
 }
 
+static void	fill_y(t_filler *filler, int j, char c)
+{
+	int	x;
+	char	a;
+
+	x = 0;
+	while (filler->territory[j][x])
+	{
+		a = filler->territory[j][x];
+		if (a == ' ')
+			filler->territory[j][x] = c;
+		++x;
+	}
+}
+
+static void	fill_x(t_filler *filler, int i, char c)
+{
+	int	y;
+	char	a;
+
+	y = 0;
+	while (y < filler->y)
+	{
+		a = filler->territory[y][i];
+		if (a == ' ')
+			filler->territory[y][i] = c;
+		++y;
+	}
+}
+
+static void	get_territory(t_filler *filler)
+{
+	int	i;
+	int	j;
+	char	c;
+	char	a;
+
+	j = 0;
+	while (j < filler->y)
+	{
+		i = 0;
+		while (i < filler->x)
+		{
+			a = filler->territory[j][i];
+			if (a != ' ' && a != '1' && a != '2')
+			{
+				c = (a == 'o' || a == 'O' || a == '1') ? '1' : '2';
+				fill_y(filler, j, c);
+				fill_x(filler, i, c);
+			}
+			++i;
+		}
+		++j;
+	}
+}
+
 void	territory_control(t_filler *filler)
 {
 	int	i;
@@ -39,6 +95,7 @@ void	territory_control(t_filler *filler)
 	j = 0;
 	ft_putstr_fd("\n", 2);
 	clean(filler);
+	get_territory(filler);
 	while (j < filler->y + 2)
 	{
 		ft_putstr_fd("|", 2);
