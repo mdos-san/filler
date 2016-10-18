7653ceb349dc774cee7b02feacd8a641220b74eb
Mickael DOS SANTOS <mdos-san@e2r6p7.42.fr>
2016-10-18 18:52:31 +0200

    New champ

--- srcs/territory_control.c
+++ srcs/territory_control.c
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/14 17:30:28 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/10/18 18:05:48 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/10/18 18:34:10 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,6 +149,6 @@ void	territory_control(t_filler *filler)
 	get_territory(filler);
 	count_territory(filler);
 	filler->nbr_r = (filler->p == 'o')
-		? ((double)filler->nbr_o)
-		: ((double)filler->nbr_x);
+		? ((double)filler->nbr_o / (double)filler->nbr_x)
+		: ((double)filler->nbr_x / (double)filler->nbr_o);
 }
