5d789a09d0d6a5292a81667770544d784f0f5bdb
Mickael DOS SANTOS <mdos-san@e2r5p6.42.fr>
2016-12-12 08:56:05 +0100

    resolving segv and memory leaks

--- srcs/filler_start.c
+++ srcs/filler_start.c
@@ -6,13 +6,13 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/13 16:26:28 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/12/06 07:05:42 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/12/12 08:53:14 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-static void	get_piece(t_filler *filler)
+static void		get_piece(t_filler *filler)
 {
 	int		i;
 	char	*buf;
@@ -34,7 +34,7 @@ static void	get_piece(t_filler *filler)
 	}
 }
 
-static void	territory_init(t_filler *filler)
+static void		territory_init(t_filler *filler)
 {
 	if (!filler->done)
 	{
@@ -44,7 +44,7 @@ static void	territory_init(t_filler *filler)
 	}
 }
 
-static int	get_board(t_filler *filler)
+static int		get_board(t_filler *filler)
 {
 	int		i;
 	char	*buf;
@@ -54,10 +54,10 @@ static int	get_board(t_filler *filler)
 	ret = get_next_line(0, &filler->board_init);
 	if (ret > 0)
 	{
-		while (filler->board_init[i] != ' ')
+		while (filler->board_init[i] != ' ' && filler->board_init[i] != '\n')
 			++i;
 		filler->y = ft_atoi(filler->board_init + ++i);
-		while (filler->board_init[i] != ' ')
+		while (filler->board_init[i] != ' ' && filler->board_init[i] != '\n')
 			++i;
 		filler->x = ft_atoi(filler->board_init + ++i);
 		i = 0;
@@ -65,6 +65,7 @@ static int	get_board(t_filler *filler)
 		{
 			get_next_line(0, &buf);
 			(i > 0) ? str_array_add(&filler->board, buf + 4) : (void)0;
+			ft_strdel(&buf);
 			++i;
 		}
 		territory_init(filler);
@@ -72,31 +73,47 @@ static int	get_board(t_filler *filler)
 	return (ret);
 }
 
-void		filler_start(t_filler *filler)
+static void		process(t_filler *filler, int *ex)
 {
 	char	*str1;
 	char	*str2;
 	char	*str3;
-	char	exit;
-	int		k;
+	char	*itoy;
+	char	*itox;
 
-	exit = 1;
-	while (exit)
+	get_new_piece(filler);
+	get_piece(filler);
+	*ex = explore(filler);
+	itox = ft_itoa(filler->cx);
+	itoy = ft_itoa(filler->cy);
+	str1 = ft_strjoin(itoy, " ");
+	str2 = ft_strjoin(str1, itox);
+	str3 = ft_strjoin(str2, "\n");
+	ft_putstr(str3);
+	ft_strdel(&itox);
+	ft_strdel(&itoy);
+	ft_strdel(&str1);
+	ft_strdel(&str2);
+	ft_strdel(&str3);
+	str_array_del(&filler->board);
+	str_array_del(&filler->piece);
+	filler->board = str_array_new();
+	filler->piece = str_array_new();
+}
+
+void			filler_start(t_filler *filler)
+{
+	int	ex;
+
+	ex = 1;
+	while (ex)
 	{
 		if (get_board(filler) > 0)
 		{
-			get_new_piece(filler);
-			get_piece(filler);
-			exit = explore(filler);
-			str1 = ft_strjoin(ft_itoa(filler->cy), " ");
-			str2 = ft_strjoin(str1, ft_itoa(filler->cx));
-			str3 = ft_strjoin(str2, "\n");
-			k = 0;
-			(filler->debug) ? ft_putstr_fd("\n", 2) : (void)0;
-			ft_putstr(str3);
-			str_array_del(&filler->board);
-			filler->board = str_array_new();
-			filler->piece = str_array_new();
+			(filler->y == 0 || filler->x == 0) ? exit(0) : (void)0;
+			process(filler, &ex);
 		}
 	}
+	str_array_del(&filler->board);
+	str_array_del(&filler->piece);
 }
